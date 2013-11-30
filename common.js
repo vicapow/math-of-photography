e27a4347f0b8612c6fd067d4c28a2ab3ca78b371
Victor Powell <vicapow@gmail.com>
2013-11-29 22:58:37 -0800

    cleaned up refelection calcuations. added normal and aor variables to intersection object

--- common.js
+++ common.js
@@ -21,17 +21,30 @@ function len(a){ return Math.sqrt(a[0]*a[0] + a[1]*a[1]) }
 function dot(a, b){ return a[0] * b[0] + a[1] * b[1] }
 function unit(a){ var l = len(a); return [ a[0] / l, a[1] / l ] }
 function projection(a, b){ return dot(a, unit(b))}
-function intersection(q1, q2, p1, p2){
-  var q = q1, s = minus(q2, q1), p = p1, r = minus(p2, p1)
-  var r_cross_x = cross(r, s)
-  if(!r_cross_x) return null // if 0, lines are parallel
+function flip(a){ return [ -a[0], -a[1] ] } // 180 rotation
+// check if the ray described by `q` (position vector) and `s` 
+// (direction vector) intersect the line segment described by vector positions
+// `p1` and `p2`
+function intersection(q, s, p1, p2){
+  var p = p1, r = minus(p2, p1), n
+  var r_cross_s = cross(r, s)
+  if(!r_cross_s) return null // if 0, lines are parallel
   var q_minus_p = minus(q, p)
   var q_minus_p_cross_r = cross(q_minus_p, r)
   if(!q_minus_p_cross_r) return null // points are on the same line
-  var t = cross(q_minus_p, s) / r_cross_x
-  var u = q_minus_p_cross_r / r_cross_x
+  var t = cross(q_minus_p, s) / r_cross_s
+  var u = q_minus_p_cross_r / r_cross_s
+  var norms = normals(p1, p2) // gives back both normals [n1, n2]
+  var r_cross_n = cross(r, norms[0])
+  // if the ray and line segment do not intersect, return null
   if( t < 0 || t > 1 || u < 0.000001 ) return null
-  return { p: add(q, scale(s, u)), u: u }
+  // if check same sign
+  if( r_cross_n * r_cross_s > 0) n = norms[0]; else n = unit(norms[1])
+  // find the normal that points in the direction of incoming intersecting ray
+  n = unit(n)
+  // calculate angle of reflection
+  var aor = minus(s, scale(n, 2 * dot(s, n)) )
+  return { position: add(q, scale(s, u)), u: u, normal: n, aor: aor }
 }
 function normals(p1, p2){
   var dx = p2[0] - p1[0]
@@ -39,29 +52,10 @@ function normals(p1, p2){
   var n1 = [-dy, dx], n2 = [dy, -dx]
   return [n1, n2]
 }
-function reflection(q1, p1, p2){
-  var line = minus(p2, p1)
-  var p = minus(q1, p1)
-  var proj = projection(p, line)
-  return add(minus(scale(unit(line), 2 * proj), p), p1)
-}
-function mirror(q, p1, p2, sect, material){
-  var n = normals(p1, p2)
-  var ac = minus(q, p1)
-  var ab = minus(p2, p1)
-  var ad = minus(p1, add(p1, n[0]))
-  var ac_cross_ab = cross(ac, ab)
-  var ad_cross_ab = cross(ad, ab)
-  ac_cross_ab = ac_cross_ab / Math.abs(ac_cross_ab)
-  ad_cross_ab = ad_cross_ab / Math.abs(ad_cross_ab)
-  if(ac_cross_ab === ad_cross_ab) n = n[0]; else n = n[1]
-  return reflection(q, sect, add(sect, n))
-}
 
 function next_rays(surfaces, ray){
   var r1 = ray[ray.length - 2]
     , r2 = ray[ray.length - 1]
-  r2 = add(r1, r2)
   var intersect = d3.merge(surfaces.map(function(surface){
     return surface.geometry.map(function(segment){
       var intersect = intersection(r1, r2, segment[0], segment[1])
@@ -73,11 +67,8 @@ function next_rays(surfaces, ray){
   })).filter(function(d){ return d }).sort(function(a, b){ return a.u - b.u })[0]
   if(!intersect) return null // no new rays! :(
 
-  var angle = mirror(r1, intersect.surface[0], intersect.surface[1]
-    , intersect.p, intersect.material)
-  angle = unit(minus(angle, intersect.p))
-
-  angle = surface_diffusion(intersect, angle)
+  // angle = surface_diffusion(intersect, angle)
+  angle = intersect.aor
   return surface_reflection(intersect, angle)
 }
 
@@ -85,9 +76,9 @@ function next_rays(surfaces, ray){
 function surface_reflection(intersect, angle){
   var reflection = intersect.material && intersect.material.reflection
   // no more bouncing around. the light should be absorbed here
-  if(rand(1) > reflection) return [[intersect.p, [0, 0]]]
+  if(rand(1) > reflection) return [[intersect.position, [0, 0]]]
   // the ray survives to to continue on its epic journey
-  return [[intersect.p, angle]]
+  return [[intersect.position, angle]]
 }
 // where `angle` is an unit vector
 var normal_diffusion = d3.random.normal(0, pi * 0.1)
